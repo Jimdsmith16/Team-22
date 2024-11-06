d75c5f98de912d76c93e9557dbb5ebefa59fafd7
Natalija Calaja <natalija.calaja@outlook.com>
2024-11-06 17:08:56 +0000

    added basket and previous order routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,16 @@ Route::get('/products', function() {
     return view('products');
 });
 
+// routing for basket page
+Route::get('/basket', function() {
+    return view('basket');
+});
+
+// routing for previous order page
+Route::get('/previous-orders', function() {
+    return view('previous-orders');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
