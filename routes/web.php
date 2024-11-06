56a96432cefc1edbbb437af57284c1b6dcf5c192
Natalija Calaja <natalija.calaja@outlook.com>
2024-11-06 08:17:30 +0000

    routes for home, about, login and products pages

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
+// routing for about us page
+Route::get('/about', function() {
+    return view('about');
+});
+
+// routing for login page
+Route::get('/login', function() {
+    return view('login');
+});
+
+// routing for product display page
+Route::get('/products', function() {
+    return view('products');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+?>
+
