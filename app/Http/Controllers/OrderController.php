8a78b7d904333e6f8e27fdc19524a79488c23f15
Natalija Calaja <natalija.calaja@outlook.com>
2024-11-19 18:34:24 +0000

    added newOrder function

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,11 +9,16 @@ use App\Models\Order;
 class OrderController extends Controller
 {
     public function show($id) {
-        $project = Order::find($id);
+        $order = Order::find($id);
         return view("previousOrders", array("order" => $order));
     }
 
     public function list() {
         return view("previousOrders", array("orders" => Order::all()));
+    }
+
+    public function newOrder(Request $request) {
+        Order::create($request->all());
+        return response()->json(['message' => 'Order added successfully.']);
     }
 }
