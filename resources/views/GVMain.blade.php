1a060301e1cdde51b559ef31b1e963c132038913
Laila <160685387+la1vi@users.noreply.github.com>
2024-11-27 10:18:58 +0000

    Update GVMain.blade.php

--- resources/views/GVMain.blade.php
+++ resources/views/GVMain.blade.php
@@ -181,18 +181,28 @@
             color: #666;
         }
 
-        footer {
-            background-color: black;
-            color: white;
-            text-align: center;
-            padding: 10px;
-            width: 100%;
-            margin-top: 40px; 
-        }
-        
-        footer p {
-            margin: 5px 0;
-        }
+ .footer {
+      background-color: #000;
+      color: #fff;
+      text-align: center;
+      padding: 15px 0;
+      margin-top: 20px;
+    }
+    
+    .footer p {
+      margin: 5px 0;
+    }
+    
+    .footer .contact-info {
+      display: flex;
+      justify-content: center;
+      gap: 15px;
+    }
+    
+    .footer .contact-info p {
+      margin: 0;
+      font-size: 0.9em;
+    }
     </style>
 </head>
 <body>
@@ -257,12 +267,14 @@
         </div>
     </div>
 
-    <footer>
-        <p>Contact us</p>
-        <p>Telephone: 0788635240</p>
-        <p>Email: GradeVault@gmail.com</p>
-        <p>Guard your Grades with GradeVault</p>
-    </footer>
+  <div class="footer">
+    <div class="contact-info">
+      <p>Contact us</p>
+      <p>Telephone: 123-456-7890</p>
+      <p>Email: info@gradevault.com</p>
+    </div>
+    <p>Guard your Grades with GradeVault</p>
+  </div>
 
     <script>
         let slideIndex = 0;
