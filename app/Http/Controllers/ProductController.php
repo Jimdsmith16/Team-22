6072ba2198f60596b16a63b2fe09c7a2104b6438
AJ <146355834+a-j16@users.noreply.github.com>
2025-01-30 23:36:19 +0000

    Update ProductController.php
    Updated to feature drop down menu for the categories

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,30 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
-    //Shows the product corresponding to the given ID.
-    public function show($id) {
-        $product = Product::find($id);
-        return view("SingleProduct", array("product" => $product));
-    }
-
-    //Shows all products.
+    // Shows all products along with categories for dropdown menu.
     public function list() {
-        return view("ProductDisplayPage", array("products" => Product::all()));
+        $products = Product::all();
+        $categories = Category::all();
+        return view("ProductDisplayPage", compact("products", "categories"));
     }
 
-    //Finds all unique products with names similar to the given name.
+    // Finds all unique products with names similar to the given name.
     public function findByName($name) {
-        $products = DB::table("products")->where("name", "like", "%" . $name. "%")->distinct()->get();
-        return view("ProductDisplayPage", array("products" => $products));
+        $products = DB::table("products")->where("name", "like", "%" . $name . "%")->distinct()->get();
+        $categories = Category::all();
+        return view("ProductDisplayPage", compact("products", "categories"));
     }
 
-    //Finds all unique products under the given category.
-    public function findByCategory($category) {
-        $categoryID = DB::table("categories")->where("name", "=", $category)->value("id");
-        $products = DB::table("products")->where("category_id", "=", $categoryID)->distinct()->get();
-        return view("ProductDisplayPage", array("products" => $products));
+    // Finds all unique products under the given category.
+    public function findByCategory(Request $request) {
+        $category = $request->input('category');
+
+        if ($category) {
+            $categoryID = DB::table("categories")->where("name", "=", $category)->value("id");
+            $products = DB::table("products")->where("category_id", "=", $categoryID)->distinct()->get();
+        } else {
+            $products = Product::all(); // If no category is selected, show all products.
+        }
+
+        $categories = Category::all();
+        return view("ProductDisplayPage", compact("products", "categories"));
     }
 }
