2b0ba436187683bcd75f153958b8ccd531ee2a00
Natalija Calaja <natalija.calaja@outlook.com>
2025-02-05 19:54:16 +0000

    added add to basket and quantity button to each single product

--- resources/views/SingleProduct.blade.php
+++ resources/views/SingleProduct.blade.php
@@ -191,6 +191,14 @@
                 <h1>{{$product->name}}</h1>
                 <div class="price">£ {{$product->price}}</div>
                 <div class="description">{{$product->description}}</div>
+
+                <form action="{{ route('basket.add') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                    <input type="number" name="quantity" value="1" min="1">
+                    <button type="submit" class="add-to-basket-btn">Add to Basket</button>
+                </form>
+
                 <button>Buy Now</button>
             </div>
         </div>
