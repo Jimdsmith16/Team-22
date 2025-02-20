92e3024d4fa4b3af573f4fdbd677e43ca340f246
Natalija Calaja <natalija.calaja@outlook.com>
2025-02-20 04:40:16 +0000

    updated Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,9 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    
-    //Sets what columns exist and can be edited.
-    protected $primaryKey = "id";
+
     public $timestamps = false;
-    protected $fillable = ['user_id', 'estimated_delivery_date','shipping_address'];
+    
+    protected $fillable = ['user_id', 'estimated_delivery_date', 'address_id'];
+
+    protected $casts = [
+        'estimated_delivery_date' => 'date',
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)
+                    ->withPivot('quantity', 'price');
+    }
 }
