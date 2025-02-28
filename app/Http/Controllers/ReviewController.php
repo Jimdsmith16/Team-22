d768e670cd1f8d601f45e29bfa1ab1b9ffc76792
Jim Smith <jimdsmith16@gmail.com>
2025-02-28 10:30:07 +0000

    Added functions to store and delete reviews along with relevant routes.

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\Review;
 
 class ReviewController extends Controller
@@ -17,5 +19,32 @@ class ReviewController extends Controller
     //Shows all reviews.
     public function list() {
         return view("/list", array("reviews" => Review::all()));
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'user_id' => 'required|numeric|exists:users,id',
+            'product_id' => 'required|integer|exists:products,id',
+            'description' => 'required|string',
+            'rating' => 'required|numeric|min:1|max:5',
+        ]);
+        Review::create([
+            'user_id' => $request->user_id,
+            'product_id' => $request->product_id,
+            'description' => $request->description,
+            'rating' => $request->rating,
+        ]);
+        return view("SingleProduct", compact(Product::find($request->product_id)));
+    }
+
+    public function destroy($id) {
+        try {
+            $review = Review::findOrFail($id);
+            $product_id = $review->product_id;
+            $review->delete();
+            return view("SingleProduct", compact(Product::find($product_id)));
+        } catch (\Exception $e) {
+            return view("ProductDisplayPage", compact(Product::all(), Category::all()));
+        }
     }
 }
